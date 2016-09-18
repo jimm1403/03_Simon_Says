bb9342352330c4d87651c438a26e98ba791c883b
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-18 13:16:26 +0200

    Add project files.

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Simon_Says
+{
+    internal class Simon
+    {
+        internal string Echo(string simonEcho)
+        {
+            return simonEcho.ToLower();
+        }
+
+        internal string Shout(string simonShout)
+        {
+            return simonShout.ToUpper();
+        }
+
+        internal string Repeat(string simonRepeat, int _repeatTimes)
+        {
+            _repeatTimes = default(int);
+            if (_repeatTimes <= 2)
+            {
+                return simonRepeat + " " + simonRepeat;
+            }
+            else
+                string.Concat(Enumerable.Repeat(simonRepeat, _repeatTimes));
+                return simonRepeat;
+            
+        }
+
+        //internal string StartOfWord(string _simonSays, int _charCount)
+        //{
+        //    return _simonSays.Substring(0, _charCount);
+        //}
+    }
+}
